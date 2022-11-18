5c2a993959e603ae12119f45727794a7eaeb7cfd
Danpa_cho <danpa725@cau.ac.kr>
2022-11-18 16:38:12 +0900

    :sparkles: feat: add `setAttributes` util to set custom attributes at html easily

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,4 +1,19 @@
+/**
+ * @param data arraylike data
+ * @param render describe rendering logic
+ */
 const map = <T>(data: T[], render: (data: T) => string) =>
     data.map(render).reduce((string, curr) => `${string}${curr}`, "")
 
-export { map }
+type Attributes<Value> = Partial<Record<string, Value>>
+/**
+ * @param attributes `DOM` attributes
+ */
+const setAttributes = (attributes: Attributes<string> | undefined): string =>
+    attributes
+        ? Object.entries(attributes)
+              .map(([key, value]) => `${key}=${value}`)
+              .reduce((attr, curr) => `${attr} ${curr}`, "")
+        : ""
+
+export { map, setAttributes }
