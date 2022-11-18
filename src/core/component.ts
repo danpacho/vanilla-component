73456d820563b9e77b9a210167c52ef60c095ee6
Danpa_cho <danpa725@cau.ac.kr>
2022-11-18 16:19:00 +0900

    :recycle: refactor: `event.listener` classes have been integrated to uphold the `Riskov substitution` principle.

--- src/core/component.ts
+++ src/core/component.ts
@@ -1,49 +1,69 @@
-import { EventListener } from "./event.listener"
-import { HTMLSetter, type TrackFunction, track } from "."
+import type { HTMLSetter } from "./core"
 
-const DEFAULT = {
-    RENDER_TARGET_ID: "app",
+type Target = HTMLDivElement
+type EventName = keyof HTMLElementEventMap
+export type EventType = HTMLElementEventMap
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type HandlerEventType = any
+type EventInfo = {
+    type: EventName
+    handler: (e: HandlerEventType) => void | unknown
+    name?: string | undefined
+    targetType?: "window" | "self" | undefined
+    targetId?: string | undefined
 }
 
-const createFragment = (template: HTMLSetter, id: string) => {
-    const fragment = document.createElement("div")
+type EventHandler = ({ target }: { target: Target }) => EventInfo
+export type EventHandlerFunction = (
+    e: HandlerEventType,
+    target?: Target
+) => void | unknown
+
+type Fragment = HTMLDivElement
+/**
+ * Get default `fragment` element. It's like react `fragment`.
+ * @note  For practical fragment role, `display: "contents"` makes it acting like non-exisisting element.
+ * @param template HTML setter function
+ * @param id fragment unique ID
+ */
+const createFragment = (
+    template: HTMLSetter,
+    id: string
+): { fragment: Fragment } => {
+    const fragment: Fragment = document.createElement("div")
     fragment.id = id
     fragment.innerHTML = template()
-    fragment.style.display = "contents" // For practical fragment role, display: "contents" makes it acting like non-exisisting element.
+    fragment.style.display = "contents"
     return {
         fragment,
     }
 }
 
-type Fragment = HTMLDivElement
+const DEFAULT_RENDER_TARGET_ID = "app"
 
-class Component extends EventListener {
-    #renderTargetID = DEFAULT.RENDER_TARGET_ID
+class Component {
+    #eventInfoList: EventInfo[] = []
+    #renderTargetID = DEFAULT_RENDER_TARGET_ID
     #fragment: Fragment
     #ref: Fragment
 
-    id: string
     template: HTMLSetter
+    id: string
 
-    constructor({
-        template,
-        renderTargetID,
-    }: {
-        template: HTMLSetter
-        renderTargetID?: string | undefined
-    }) {
+    constructor({ template }: { template: HTMLSetter }) {
         const id = window.crypto.randomUUID()
         const { fragment } = createFragment(template, id)
-
-        super(fragment)
 
         this.#fragment = fragment
         this.#ref = this.#fragment
 
         this.id = id
         this.template = template
+    }
 
-        if (renderTargetID) this.#renderTargetID = renderTargetID
+    #updateRenderTargetID(newID: string) {
+        this.#renderTargetID = newID
     }
 
     /**
@@ -54,10 +74,12 @@ class Component extends EventListener {
     }
 
     /**
-     * Mount fragment DOM element at `this.#renderTargetID`
+     * Mount `fragment` `DOM` element at `this.#renderTargetID`
      */
     #mount(renderTargetID?: string) {
-        this.#renderTargetID = renderTargetID ?? DEFAULT.RENDER_TARGET_ID
+        if (renderTargetID) {
+            this.#updateRenderTargetID(renderTargetID)
+        }
         const target = document.getElementById(this.#renderTargetID)
         target?.appendChild(this.#fragment)
 
@@ -66,13 +88,16 @@ class Component extends EventListener {
 
     /**
      * Update `HTML` template in fragment
+     * @note core functionality of updating `DOM`.
      */
-    updateHTML() {
+    updateDOM() {
         this.#fragment.innerHTML = this.template()
         this.#updateRef()
     }
+
     /**
-     * Use fragment element when mounted
+     * Use fragment element, when mounted to the `DOM` tree.
+     * @note should be called after `render` or `staticRender`
      * @param mountedCallback get mounted fragment as first argument
      * @example
      * .onMounted((fragment) => {
@@ -98,27 +123,88 @@ class Component extends EventListener {
     }
 
     /**
-     * Effect is track siginal variables.
-     * Whenever signal changes, effectFunction will be executed.
-     */
-    effect(effectFuntion: TrackFunction) {
-        this.render()
-        track(effectFuntion)
-        return this
-    }
-
-    /**
-     * Get bulk DOM reference. Same as current fragment, but it is not mounted in DOM tree.
+     * Get `fragment` reference.
+     * @note Same as current fragment, but it is not mounted in `DOM` tree.
      */
     ref() {
         return this.#ref
     }
 
+    #updateEventHandler(
+        updateIndex: number,
+        updatedHandler: EventInfo["handler"]
+    ) {
+        const updatedEventInfo: EventInfo = {
+            ...(this.#eventInfoList[updateIndex] as EventInfo),
+            handler: updatedHandler,
+        }
+        this.#eventInfoList.splice(updateIndex, 1, updatedEventInfo)
+    }
+
     /**
-     * Get static `HTML` component. Use it for static components.
+     * Add eventlistner
+     * @param eventHandler returns `evntInfo` that is stored in specific component
      */
-    html(renderTargetID?: string) {
-        this.#mount(renderTargetID)
+    addEvent(eventHandler: EventHandler) {
+        const eventInfo = eventHandler({ target: this.#fragment })
+        const { handler, type, name } = eventInfo
+
+        this.#eventInfoList.push({
+            name,
+            type,
+            handler,
+            targetType: eventInfo.targetType,
+            targetId: eventInfo.targetId,
+        })
+
+        return this
+    }
+
+    /**
+     * Mount event at the `fragment`
+     * @note sync event with component rendering cycles.
+     */
+    mountEvent() {
+        this.#eventInfoList.forEach((eventInfo, index) => {
+            const { type } = eventInfo
+            if (eventInfo.targetType === "window") {
+                const handler = (e: HandlerEventType) => {
+                    eventInfo.handler(e)
+                }
+                this.#updateEventHandler(index, handler)
+
+                window.addEventListener(type, handler)
+                return this
+            } else {
+                const handler = (e: HandlerEventType) => {
+                    if (eventInfo?.targetId) {
+                        if (
+                            (e.target as HTMLElement).id === eventInfo.targetId
+                        ) {
+                            eventInfo.handler(e)
+                        }
+                    } else {
+                        eventInfo.handler(e)
+                    }
+                }
+                this.#updateEventHandler(index, handler)
+
+                this.#fragment.addEventListener(type, handler)
+                return this
+            }
+        })
+    }
+
+    /**
+     * Remove specific eventw
+     * @param targetName remove event target name
+     */
+    removeEvent(targetName: string) {
+        const removedEventInfoList = this.#eventInfoList.filter(
+            (e) => e.name !== targetName
+        )
+        this.#eventInfoList = removedEventInfoList
+        return this
     }
 }
 
